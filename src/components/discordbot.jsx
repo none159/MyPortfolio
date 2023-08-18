e9483f6c5084e1506376ccb8b0797284811cf139
none159 <noneenone019@gmail.com>
2023-08-18 07:10:56 +0100

    first commit

--- src/components/discordbot.jsx
+++ src/components/discordbot.jsx
@@ -1,10 +1,8 @@
-import React, { useLayoutEffect } from "react";
+import React from "react";
 import { Link } from 'react-router-dom'
 
 const Discord = () => {
-    useLayoutEffect(() => {
-        window.scrollTo(0, 0)
-    });
+
     return (
         <section>
             <div className="details-container">
